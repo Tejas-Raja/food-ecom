fe6741294568dab47383766e9177ae7f3a5fdf97
Tejas Raja <tejasraja4@gmail.com>
2025-07-09 18:12:53 +0530

    added frontend user registration and login functionalities

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,23 @@
-import React, { useContext, useState } from 'react'
+import React, { Profiler, useContext, useState } from 'react'
 import './navbar.css'
 import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { StoreContext } from '../../context/StoreContext'
 
 
 const navbar = ({ setShowLogin }) => {
 
   const [menu, setmenu] = useState("home");
-  const { getTotalCartAmount } = useContext(StoreContext);
+
+  const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
+
+  const navigate = useNavigate;
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    navigate("/");
+  }
   return (
     <div className='navbar'>
       <Link to='/'><img src={assets.logo} alt="" className='logo' /></Link>
@@ -25,7 +34,16 @@ const navbar = ({ setShowLogin }) => {
           <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
           <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
-        <button onClick={() => setShowLogin(true)}>sign in</button>
+        {!token ? <button onClick={() => setShowLogin(true)}>sign in</button>
+          : <div className='navbar-profile'>
+            <img src={assets.profile_icon} alt="" />
+            <ul className="nav-profile-dropdown">
+              <li><img src={assets.bag_icon} alt="" /><p>orders</p></li>
+              <hr />
+              <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>logout</p></li>
+            </ul>
+          </div>}
+
       </div>
     </div>
   )
