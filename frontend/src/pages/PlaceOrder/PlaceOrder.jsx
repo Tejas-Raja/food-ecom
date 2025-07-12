9b71aa32b3a2c1871146efff1a428924ea598566
Tejas Raja <tejasraja4@gmail.com>
2025-07-12 20:07:51 +0530

    added payment functionality with stripe

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,28 +1,73 @@
 import React, { useContext } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext';
+import { useState } from 'react';
+import axios from 'axios';
 
 const PlaceOrder = () => {
-  const { getTotalCartAmount } = useContext(StoreContext);
+  const { getTotalCartAmount, token, food_list, cartItems, url } = useContext(StoreContext);
+
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: ""
+  })
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data => ({ ...data, [name]: value }));
+  }
+
+  const placeOrder = async (event) => {
+    event.preventDefault();
+    let orderItems = [];
+    food_list.map((item) => {
+      if (cartItems[item._id] > 0) {
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItems[item._id];
+        orderItems.push(itemInfo);
+      }
+    })
+    let orderData = {
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount() + 2,
+    }
+    let response = await axios.post(url + "/api/order/place", orderData, { headers: { token } });
+    if (response.data.success) {
+      const { session_url } = response.data;
+      window.location.replace(session_url);
+    } else {
+      alert("Error");
+    }
+  }
+
   return (
-    <form className='place-order'>
+    <form onSubmit={placeOrder} className='place-order'>
       <div className="place-order-left">
         <p className="title">delivery information</p>
         <div className="multi-fields">
-          <input type="text" placeholder='first name' />
-          <input type="text" placeholder='last name' />
+          <input required name='firstName' onChange={onChangeHandler} value={data.firstName} type="text" placeholder='first name' />
+          <input required name='lastName' onChange={onChangeHandler} value={data.lastName} type="text" placeholder='last name' />
         </div>
-        <input type="email" placeholder='email address' />
-        <input type="text" placeholder='street' />
+        <input required name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='email address' />
+        <input required name='street' onChange={onChangeHandler} value={data.street} type="text" placeholder='street' />
         <div className="multi-fields">
-          <input type="text" placeholder='city' />
-          <input type="text" placeholder='state' />
+          <input required name='city' onChange={onChangeHandler} value={data.city} type="text" placeholder='city' />
+          <input required name='state' onChange={onChangeHandler} value={data.state} type="text" placeholder='state' />
         </div>
         <div className="multi-fields">
-          <input type="text" placeholder='zip code' />
-          <input type="text" placeholder='country' />
+          <input required name='zipcode' onChange={onChangeHandler} value={data.zipcode} type="text" placeholder='zip code' />
+          <input required name='country' onChange={onChangeHandler} value={data.country} type="text" placeholder='country' />
         </div>
-        <input type="text" placeholder='phone' />
+        <input required name='phone' onChange={onChangeHandler} value={data.phone} type="text" placeholder='phone' />
       </div>
       <div className="place-order-right">
         <div className="cart-total">
@@ -43,7 +88,7 @@ const PlaceOrder = () => {
               <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
             </div>
           </div>
-          <button>proceed to payment</button>
+          <button type='submit'>proceed to payment</button>
         </div>
       </div>
     </form>
