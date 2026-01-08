c622842d384bff7a65d33df2cdb48adb8ebefa0c
Tejas Raja <tejasraja4@gmail.com>
2026-01-08 12:17:27 +0530

    edits

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose.connect('mongodb+srv://tejas:DhECu1aYUcTPwJeh@cluster0.qevp9mw.mongodb.net/food-del').then(() => console.log("DB connected"));
+  await mongoose.connect('mongodb+srv://tejas:database@cluster0.qevp9mw.mongodb.net/food-del').then(() => console.log("DB connected"));
 }
